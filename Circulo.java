a4778e7a8f26e4d5cefb90111ff4ac6b86996a8d
Jesus <chechubui@hotmail.com>
2014-10-29 10:48:32 +0100

    este es un ejercicio del circulo

---
+++ Circulo.java
@@ -0,0 +1,15 @@
+public class Circulo {
+	
+	public static void main(String[] args) {
+	
+	double radio; // Declaramos el radio
+	double area; // Declaramose el area
+	//Asignamos el radio
+		radio = 20; // nuevo valor para el radio
+	// Computamos el area
+		area = radio * radio * 3.14159;
+	// Mostramos los resultados
+	
+	System.out.println("El area del circulo de radio " + radio + " es " + area);
+}
+}
